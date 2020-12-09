a96eb828df8277ee6f4a39ba11d66a867baffcdf
EvenDebauge <74670046+EvenDebauge@users.noreply.github.com>
2020-12-09 16:34:06 +0100

    Add files via upload

--- motus.py
+++ motus.py
@@ -3,44 +3,57 @@ init()
 from colorama import Fore,Back,Style
 import random
 
-def bibliotheque_mot (liste):
-    bibliotheque_mot=[wapiti,webcam,totoro,atouts,manger,orange,castor,bureau,phyton,chaton]
-    choix_mot= random.randint(0.9)
-    mot_choisis=bibliotheque_mot[choix_mot]
-    tour=1
-    
 
-def mot_joueur (mot_aleatoire):
-    print(STYLE.RESET_ALL)
-    mot_joueur= input("Entrez un mot de six lettres:")
-    if mot_choisis[i] == mot_joueur[i]:
-        print(Back.RED +mot_joueur[i], end="")
-    else:
-        print(Back.BLUE + mot_joueur[i], end="")
-    for i in range(0,7):
-        return mot_joueur
-
-
-
-
-
-
-
-    
-def rules (regles):
+def rules():
     print("Le jeu choisira un mot aléatoire parmis une liste de 10 mots")
     print("Si une lettre est bien placée par rapport au mot à trouver, elle sera rouge.")
     print("Si une lettre est mal placée par rapport au mot à trouver, elle sera jaune.")
     print("Si une lettre ne se trouve pas dans le mot à trouver, elle sera bleue.")
     print("Le mot à trouver est un mot de six lettres")
     print("_ _ _ _ _ _")
+    
+    
+def tourJoueur (motAleatoire):
+    print(Style.RESET_ALL)
+    motJoueur= input("Entrez un mot de six lettres: ")
+    for i in range (0,6):
+        if motChoisis[i] == motJoueur[i]:
+            print(Back.RED +motJoueur[i], end="")
+        else:
+            print(Back.BLUE + motJoueur[i], end="")
+            print(Style.RESET_ALL)
+    return motJoueur
 
 def test_victoire (test):
-    if mot_choisis == mot_joueur :
+    if motChoisis == motJoueur :
         victoire =True
-        print("Vous avez trouvés!")
+        print("   Vous avez trouvés!")
     else: 
         victoire= False
-        print("Il vous reste",tentatives,".")
-        tentatives=tentatives-1
+        print("  Il vous reste",tentatives_restantes,".")
     return victoire
+    
+def test_defaite (test):
+    if tentatives == 9:
+        defaite=True
+        print("Vous n'avez plus de tentatives, vous avez échoués.")
+    else:
+        defaite=False
+        
+bibliotheque_mot=["wapiti","webcam","totoro","atouts","manger","orange","castor","bureau","phyton","chaton"]
+choix_mot= random.randint(0,9)
+motChoisis=bibliotheque_mot[choix_mot]
+tentatives=1
+tentatives_restantes=7
+victoire=False
+defaite=False
+rules()
+while tentatives<9 and victoire != True:
+    print(Style.RESET_ALL)
+    print("tentative n°",tentatives,".")
+    motJoueur=tourJoueur(motChoisis)
+    victoire=test_victoire(motJoueur)
+    defaite=test_defaite(motJoueur)
+    tentatives=tentatives+1
+    tentatives_restantes=tentatives_restantes-1
+
